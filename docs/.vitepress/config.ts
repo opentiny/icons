608011fb26178327414878d18be473931fcfe220
shenjunjian <40288193@qq.com>
2025-03-28 23:30:11 -0700

    fix(logo): 修复logo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: " ",
+  title: "Opentiny Design",
   description: "A Cross-framework Icons Library",
   base: "/icons/",
   cleanUrls: true,
